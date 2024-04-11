331a9960f8c813a7e809686b30b92fe591475197
anik-saha-2001 <aniksahamda@gmail.com>
2024-04-11 19:21:35 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import connectDB from "../db/index.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`Server is running on port ${3000}!!!`);
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed: ", err);
+  });
