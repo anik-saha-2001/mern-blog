c8d313956a0c55be5cd977fda123a8a83763b284
anik-saha-2001 <aniksahamda@gmail.com>
2024-04-20 12:41:55 +0530

    add create post api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 
 dotenv.config();
 const app = express();
@@ -22,6 +23,7 @@ connectDB()
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 // middleware
 app.use((err, req, res, next) => {
