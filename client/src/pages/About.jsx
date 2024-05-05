8c03e5ed2c5f73aebb8cab78e35ad1ba266f2bfb
anik-saha-2001 <aniksahamda@gmail.com>
2024-05-05 20:45:42 +0530

    about and project page done

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,28 @@
 const About = () => {
-  return <div className="text-3xl">About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Anik's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Anik's Blog is a Blog website that I created in MERN stack along
+              with flowbite, tailwind css, react icons, react-quill,etc for
+              styling, redux, redux toolkit and redux persist for state
+              management, firebase for authentication and image upload. Where as
+              in backend used dotenv, cookie-parser, jwt, bcryptjs.
+            </p>
+            <p>
+              I am learning full stack web development. Hope you enjoy going
+              through this project and read my blogs.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,13 @@
+import CallToAction from "../components/CallToAction";
+
 const Projects = () => {
-  return <div className="text-3xl">Projects</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-3 mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-5">Projects</h1>
+      <p className="mb-5 text-md">Project links coming soon...</p>
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Projects;
